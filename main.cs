e324770f7abbcb5fa7259209955904309d0af149
hrapaeva <104451032+hrapaeva@users.noreply.github.com>
2022-10-04 19:49:10 +0300

    Update main.cs

--- main.cs
+++ main.cs
@@ -7,12 +7,12 @@ class Program {
       Console.WriteLine("Введите y: ");
       double y = Convert.ToDouble(Console.ReadLine());
 
-      if (Math.Abs(x+y) + Math.Abs(x-y) <= 10) {
-      if ((y >= 2*x - 5) && (y <= 2*x + 5) && (x*x + y*y <= 25)) { Console.WriteLine($"Точка находится в закрашенной области"); }
-      else if ((x <= 0) && (y >=0) && (x*x + y*y >= 25)) { Console.WriteLine($"Точка находится в закрашенной области"); }
-      else if ((x <= 0) && (y <= 0) && (x*x + y*y >= 25) && (y >= 2*x + 5)) { Console.WriteLine($"Точка находится в закрашенной области"); }
-      else if ((x >= 0) && (y <= 0) && (x*x + y*y >=25)) { Console.WriteLine($"Точка находится в закрашенной области"); }
-      else { Console.WriteLine($"Точка вне закрашенной области"); }
+      if (Math.Abs(x+y) + Math.Abs(y -x) <= 10) {
+      if ((y >= 2*x - 5) & (y <= 2*x + 5) & (x*x + y*y <= 25)) Console.WriteLine($"Точка находится в закрашенной области"); 
+      else if ((x <= 0) & (y >=0) & (x*x + y*y >= 25)) Console.WriteLine($"Точка находится в закрашенной области"); 
+      else if ((x <= 0) & (y <= 0) & (x*x + y*y >= 25) & (y >= 2*x + 5))  Console.WriteLine($"Точка находится в закрашенной области");
+      else if ((x >= 0) & (y <= 0) & (x*x + y*y >=25)) Console.WriteLine($"Точка находится в закрашенной области"); 
+      else  Console.WriteLine($"Точка вне закрашенной области"); 
       }
       else  Console.WriteLine($"Точка вне данной области");
 }
